f6dd7ac999e798ce091bda334ae1deb086c9a62e
Dalo-Chinkhwangwa-Prof <Dalo.chinkhwangwa@outlook.com>
2021-04-22 11:23:23 -0400

    Into to ConstraintLayout

--- app/src/main/java/com/coolcats/myapplication/MainActivity.java
+++ app/src/main/java/com/coolcats/myapplication/MainActivity.java
@@ -33,11 +33,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         calculatorView = findViewById(R.id.output_textview);
     }
-
-
 
     public void onClick(View view){
 
